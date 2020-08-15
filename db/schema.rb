29bfdce53a64ce327c0eb924ed5ac6f1290ce978
山田翔太 <yamada-shouta@yamadashoutanoMacBook-Pro.local>
2020-08-15 11:00:49 +0900

    creat User_model with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_140219) do
+ActiveRecord::Schema.define(version: 2020_08_15_004358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(version: 2020_08_14_140219) do
 
   create_table "learning_methods", force: :cascade do |t|
     t.string "method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "nickname", null: false
+    t.string "image"
+    t.string "encrypted_password", default: "", null: false
+    t.integer "language_id"
+    t.integer "work_method_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
