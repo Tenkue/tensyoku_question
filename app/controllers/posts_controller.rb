ec618962db51fdc61ae176bcfbeb012bcc108b8e
abeyasumasa <abeyasumasa@gmail.com>
2020-09-01 23:29:26 +0900

    Add_Post_controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  #新規投稿
+  def new
+  end
+
+  def create
+  end
+#投稿詳細
+  def show
+  end
+#投稿編集
+  def edit
+  end
+
+  def update
+  end
+#投稿削除
+  def destroy
+  end
+end
