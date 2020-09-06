77779762236d0a0e2791d45f34a109b7817edd72
阿部靖真 <abeyasumasa@MacBook-Air.local>
2020-09-07 07:37:52 +0900

    develop/Add_new_post_function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,19 @@ class PostsController < ApplicationController
   #新規投稿
   def new
     @post = Post.new
-    @style = Style.new
-    @place = Place.new
-    @question = Question.new
+    # @post.build_style
+    # @post.build_place
+    # @questions = Question.all
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      puts '投稿しました'
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 #投稿詳細
   def show
@@ -20,5 +27,18 @@ class PostsController < ApplicationController
   end
 #投稿削除
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(
+      :interview_date,
+      :user_id,
+      :place_id,
+      :style_id,
+      # style_attributes: [:id,
+      #                    :business_form],
+      # place_attributes: [:id,
+      #                    :work_location],
+      question_ids: [])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :place
   belongs_to :style
-  has_many :post_questions, through: :post_questions
+  has_many :post_questions, dependent: :destroy
+  has_many :questions, through: :post_questions
+  # accepts_nested_attributes_for :style
+  # accepts_nested_attributes_for :place
 end
