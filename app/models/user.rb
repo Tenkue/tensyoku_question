ffacd0cb14495f2c0d022a90d5fb9162ce77ebfb
山田翔太 <yamada-shouta@yamadashoutanoMacBook-Pro.local>
2020-09-21 21:28:34 +0900

    fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :languages, through: :user_languages
   belongs_to :learning_method
   devise :database_authenticatable, :recoverable, :rememberable,:registerable, :authentication_keys => [:nickname]
-  accepts_nested_attributes_for :language,:learning_method
+  accepts_nested_attributes_for :learning_method
   mount_uploader :image, ImageUploader
 end
