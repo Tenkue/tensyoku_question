e608293b2274264694961fdbbb28b0c5dda3fe68
山田翔太 <yamada-shouta@yamadashoutanoMacBook-Pro.local>
2020-09-21 20:16:44 +0900

    fix user_edit_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 class UsersController < ApplicationController
+  before_action :find_user   
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
     user.update(user_params)
     redirect_to action: :show
   end
@@ -16,5 +14,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.permit(:nickname, :age, :image)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
