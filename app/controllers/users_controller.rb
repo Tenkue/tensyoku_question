39fd17c1a58ffb3532eeeca3d36353469dd0f913
山田翔太 <yamada-shouta@yamadashoutanoMacBook-Pro.local>
2020-09-21 22:19:40 +0900

    add user_edit_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    user.update(user_params)
+    @user.update(user_params)
     redirect_to action: :show
   end
 
   private
   def user_params
-    params.permit(:nickname, :age, :image)
+    params.require(:user).permit(
+      :nickname, 
+      :age, 
+      :image, 
+      :learning_method_id, 
+      language_ids: [])
   end
 
   def find_user
